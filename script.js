d0814fdef13380dd83efe44194d692ebe17bb2ee
raoismailqadri <raoismailqadri@gmail.com>
2024-06-26 14:59:00 +0500

    new changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const apiKey = 'e533a71aa3b1096eee2e821ba69652f8'; // Your OpenWeatherMap API key
 
 const fetchWeather = () => {
-  const city = document.getElementById('location-input').value;
+  const cityInput = document.getElementById('location-input');
+  const city = cityInput.value;
   if (!city) {
     alert('Please enter a city name');
     return;
@@ -15,17 +16,23 @@ const fetchWeather = () => {
       return response.json();
     })
     .then(data => {
-        document.getElementById('city').textContent = data.name;
-        document.getElementById('sun_rise').textContent = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
-        document.getElementById('description').textContent = data.weather[0].description;
-        document.getElementById('temperature').textContent = data.main.temp;
-        document.getElementById('feel_like').textContent = data.main.feels_like;
-        document.getElementById('humidity').textContent = data.main.humidity + '%';
-        document.getElementById('clouds').textContent = data.clouds.all + '%';
-        document.getElementById('rain').textContent = data.rain ? data.rain['1h'] + 'mm' : '0mm';
+      document.getElementById('city').textContent = data.name;
+      document.getElementById('sun_rise').textContent = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+      document.getElementById('description').textContent = data.weather[0].description;
+      document.getElementById('temperature').textContent = data.main.temp;
+      document.getElementById('feel_like').textContent = data.main.feels_like;
+      document.getElementById('humidity').textContent = data.main.humidity + '%';
+      document.getElementById('clouds').textContent = data.clouds.all + '%';
+      document.getElementById('rain').textContent = data.rain ? data.rain['1h'] + 'mm' : '0mm';
+      
+      // Clear the input field after successfully fetching and displaying the weather data
+      cityInput.value = '';
     })
     .catch(error => {
       console.error('There has been a problem with your fetch operation:', error);
       alert('Unable to find location. Please write the correct Name.');
+      
+      // Clear the input field even if there was an error
+      cityInput.value = '';
     });
 };
